f6782ae92f2cc001bcc8f5bbd40180c12d925f37
alexandre menezes <alexandre.fmenezes@stf.jus.br>
2019-11-06 12:49:12 -0300

    release version 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rabbit-client",
-    version="0.2.1a3",
+    version="0.2.1",
     author="alexandre menezes",
     author_email="alexandre.fmenezes@gmail.com",
     description="rabbit message queue client",
